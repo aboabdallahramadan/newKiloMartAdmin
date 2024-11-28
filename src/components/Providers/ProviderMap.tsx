549548a6258535a4c425cbcac715623068782407
mohammad ramadan <aboabdallahramadan@gmail.com>
2024-11-28 18:03:57 +0300

    fixed map z-index bug

--- src/components/Providers/ProviderMap.tsx
+++ src/components/Providers/ProviderMap.tsx
@@ -37,7 +37,7 @@ const ProviderMap = () => {
       {loading ? (
         <ElementLoader />
       ) : (
-        <MapContainer center={location} zoom={13} style={{ height: "400px", width: "100%" }}>
+        <MapContainer center={location} zoom={13} style={{ height: "400px", width: "100%" }} className='z-0'>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'

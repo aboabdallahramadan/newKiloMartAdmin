8269cf8d61abab4257a170064352071da64f5b89
mohammad ramadan <aboabdallahramadan@gmail.com>
2025-01-16 16:24:18 +0300

    got the real dataState

--- src/components/DataStats/DataStatsOne.tsx
+++ src/components/DataStats/DataStatsOne.tsx
@@ -2,46 +2,41 @@
 import React, {useState , useEffect} from "react";
 import { dataStats } from "@/types/dataStats";
 import { BsFillCartFill } from "react-icons/bs";
-import { OrderCount } from "@/types/orderCount";
-import { useSession } from "next-auth/react";
-
+import ElementLoader from "../common/ElementLoader";
 
 
 const DataStatsOne: React.FC<dataStats> = () => {
-  const { data: session } = useSession();
-  
-
-  const [ordersData, setOrdersData] = useState<OrderCount>(
-
-      {
-        totalOrdersCount: 0,
-        profit: 0,
-      },
-
-  );
+  const [ordersCount, setOrdersCount] = useState<number>(0);
+  const [profit, setProfit] = useState<number>(0);
+  const [offersCount, setOffersCount] = useState<number>(0);
+  const [customersCount, setCustomersCount] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const fetchOrders = async () => {
+    const fetchAllData = async () => {
       
       try {
-        const response = await fetch('/backend/api/admin-panel/order-count', {
-          headers: {
-            'Authorization': `Bearer ${session?.accessToken}`,
-          }
-        });
+        const response = await fetch('/backend/api/admin-panel/order-users-offers-count');
         const res = await response.json();
         if(res.status == true){
           const data = res.data;
-          setOrdersData(data);
+          setOrdersCount(data.totalOrdersCount);
+          setProfit(data.profit);
+          setOffersCount(data.offersCount);
+          setCustomersCount(data.customers.total);
         }
         else{
           console.log(res.message);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
-    fetchOrders();
+
+    fetchAllData();
+
   }, []);
   const dataStatsList = [
     {
@@ -50,7 +45,7 @@ const DataStatsOne: React.FC<dataStats> = () => {
       ),
       color: "#3FD97F",
       title: "Total Orders",
-      value: ordersData.totalOrdersCount,
+      value: ordersCount,
       growthRate: 0.43,
     },
     {
@@ -72,7 +67,7 @@ const DataStatsOne: React.FC<dataStats> = () => {
       ),
       color: "#FF9C55",
       title: "Total Profit",
-      value: ordersData.profit + " SAR",
+      value: profit + " SAR",
       growthRate: 4.35,
     },
     {
@@ -100,7 +95,7 @@ const DataStatsOne: React.FC<dataStats> = () => {
       ),
       color: "#8155FF",
       title: "Total Product",
-      value: "250",
+      value: offersCount,
       growthRate: 2.59,
     },
     {
@@ -138,7 +133,7 @@ const DataStatsOne: React.FC<dataStats> = () => {
       ),
       color: "#18BFFF",
       title: "Total Customers",
-      value: "365",
+      value: customersCount,
       growthRate: -0.95,
     },
   ];
@@ -150,58 +145,67 @@ const DataStatsOne: React.FC<dataStats> = () => {
             key={index}
             className="rounded-[10px] bg-white p-6 shadow-1 dark:bg-gray-dark"
           >
-            <div
-              className="flex h-14.5 w-14.5 items-center justify-center rounded-full"
-              style={{ backgroundColor: item.color }}
-            >
-              {item.icon}
-            </div>
+            {
+              isLoading ? (
+                <ElementLoader />
+              ) : (
+                <>
+                <div
+                  className="flex h-14.5 w-14.5 items-center justify-center rounded-full"
+                  style={{ backgroundColor: item.color }}
+                >
+                  {item.icon}
+                </div>
 
-            <div className="mt-6 flex items-end justify-between">
-              <div>
-                <h4 className="mb-1.5 text-heading-6 font-bold text-dark dark:text-white">
-                  {item.value}
-                </h4>
-                <span className="text-body-sm font-medium">{item.title}</span>
-              </div>
+                <div className="mt-6 flex items-end justify-between">
+                  <div>
+                    <h4 className="mb-1.5 text-heading-6 font-bold text-dark dark:text-white">
+                      {item.value}
+                    </h4>
+                    <span className="text-body-sm font-medium">{item.title}</span>
+                  </div>
 
-              {/* <span
-                className={`flex items-center gap-1.5 text-body-sm font-medium ${
-                  item.growthRate > 0 ? "text-green" : "text-red"
-                }`}
-              >
-                {item.growthRate}%
-                {item.growthRate > 0 ? (
-                  <svg
-                    className="fill-current"
-                    width="10"
-                    height="10"
-                    viewBox="0 0 10 10"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                  {/* <span
+                    className={`flex items-center gap-1.5 text-body-sm font-medium ${
+                      item.growthRate > 0 ? "text-green" : "text-red"
+                    }`}
                   >
-                    <path
-                      d="M4.35716 2.3925L0.908974 5.745L5.0443e-07 4.86125L5 -5.1656e-07L10 4.86125L9.09103 5.745L5.64284 2.3925L5.64284 10L4.35716 10L4.35716 2.3925Z"
-                      fill=""
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="fill-current"
-                    width="10"
-                    height="10"
-                    viewBox="0 0 10 10"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M5.64284 7.6075L9.09102 4.255L10 5.13875L5 10L-8.98488e-07 5.13875L0.908973 4.255L4.35716 7.6075L4.35716 7.6183e-07L5.64284 9.86625e-07L5.64284 7.6075Z"
-                      fill=""
-                    />
-                  </svg>
-                )}
-              </span> */}
-            </div>
+                    {item.growthRate}%
+                    {item.growthRate > 0 ? (
+                      <svg
+                        className="fill-current"
+                        width="10"
+                        height="10"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M4.35716 2.3925L0.908974 5.745L5.0443e-07 4.86125L5 -5.1656e-07L10 4.86125L9.09103 5.745L5.64284 2.3925L5.64284 10L4.35716 10L4.35716 2.3925Z"
+                          fill=""
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        className="fill-current"
+                        width="10"
+                        height="10"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M5.64284 7.6075L9.09102 4.255L10 5.13875L5 10L-8.98488e-07 5.13875L0.908973 4.255L4.35716 7.6075L4.35716 7.6183e-07L5.64284 9.86625e-07L5.64284 7.6075Z"
+                          fill=""
+                        />
+                      </svg>
+                    )}
+                  </span> */}
+                </div>
+                </>
+              )
+            }
+            
           </div>
         ))}
       </div>

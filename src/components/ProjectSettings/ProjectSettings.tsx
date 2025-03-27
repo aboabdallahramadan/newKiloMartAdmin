bf791d238599cab241f245fdbd504fc37a76a23b
mohammad ramadan <aboabdallahramadan@gmail.com>
2025-03-27 04:33:50 +0300

    fixed the not changable circle radius bug in the project settings

--- src/components/ProjectSettings/ProjectSettings.tsx
+++ src/components/ProjectSettings/ProjectSettings.tsx
@@ -118,7 +118,7 @@ const ProjectSettings = () => {
               Circle Radius
               <input
                 type="number"
-                name="circleRadius"
+                name="circleRaduis"
                 value={settings.circleRaduis}
                 onChange={handleChange}
                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
